6f12070cc03ba118980bc9553beec45ddfc2f696
Nikolay Sergievskiy <nikolay@xix.ai>
2020-06-17 13:51:24 +0300

    tsd

--- configs/tsd/tsd_ga_rcnn_r50_fpn_dcn.py
+++ configs/tsd/tsd_ga_rcnn_r50_fpn_dcn.py
@@ -162,7 +162,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 600),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
