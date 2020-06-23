9914444f39d7409758686ef5eb05720a688e2f70
U-DESKTOP-A6FGL28\nekit <mm95@rice.edu>
2020-06-23 02:09:03 -0500

    Correct BBaug for distr train

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -7,7 +7,6 @@ from numpy import random
 from mmdet.core import PolygonMasks
 from mmdet.core.evaluation.bbox_overlaps import bbox_overlaps
 from ..builder import PIPELINES
-from .compose import Compose as MMCompose
 
 try:
     import bbaug
