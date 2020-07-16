125a8809aa142e9a2f4ba4a97bbb5a745c61c845
U-DESKTOP-A6FGL28\nekit <mm95@rice.edu>
2020-07-16 01:33:35 -0500

    Add mosaic examples

---
+++ configs/mosaic_examples/mask_rcnn_r50_fpn_mosaic_v1_1x_coco.py
@@ -0,0 +1,19 @@
+_base_ = '../mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='MosaicV1', buffer_add_probability = 0.5, max_buffer_size = 20, crop_probability = 0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(
+        type='Collect',
+        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks'],
+        meta_keys=('filename', 'ori_shape', 'img_shape', 'img_norm_cfg',
+                   'pad_shape', 'scale_factor'))
+]
+data = dict(train=dict(pipeline=train_pipeline))
